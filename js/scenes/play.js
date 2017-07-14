76f1dbb91fa22681c6583661cd423a1712fcc25b
Armag3ddon <Armag3ddon@web.de>
2017-07-14 22:52:43 +0200

    hui

--- js/scenes/play.js
+++ js/scenes/play.js
@@ -1,10 +1,18 @@
-define(['lib/scene', 'geo/v2', 'entity/controller'],
-		function(Scene, V2, Controller ) {
+define(['lib/scene', 'geo/v2', 'core/graphic', 'entity/controller', 'entity/typefield'],
+		function(Scene, V2, g, Controller, TypeField ) {
+			g.add('img/background.jpg');
+
 			function PlayScene() {
 				Scene.call(this);
 
 				this.controller = new Controller(Zero());
 				this.add(this.controller);
+
+				this.typefield = new TypeField(new V2(200, 650));
+				this.add(this.typefield);
+				this.keyAware.push(this.typefield);
+
+				this.bg = 'img/background.jpg';
 			}
 
 			PlayScene.prototype = new Scene();
