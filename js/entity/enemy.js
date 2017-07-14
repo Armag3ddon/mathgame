d78f82e343717d3a0ef0dbe2e02f3f96c7bc2a28
Michael Lennartz <michael@dubspeed.de>
2017-07-14 23:41:03 +0200

    add text

--- js/entity/enemy.js
+++ js/entity/enemy.js
@@ -1,12 +1,16 @@
-define(['basic/rect', 'geo/v2'],
-    function(Rect, V2)
+define(['basic/rect', 'geo/v2', 'basic/text', 'config/fonts'],
+    function(Rect, V2, TextEntity, f)
     {
         function Enemy(pos, size, color, options) {
             Rect.call(this, pos, size, color);
 
-            this.options = options;
+            var font = f.onscreen;
 
-            //console.log('spawned new enemy ', this.options);
+            this.options = options;
+            this.text = [options.op_1,options.operator, options.op_2].join('');
+            this.display = new TextEntity(Zero(), this.text, font);
+
+            this.add(this.display);
         }
 
         Enemy.prototype = new Rect();
@@ -17,6 +21,10 @@ define(['basic/rect', 'geo/v2'],
 
             this.parent.checkEntityForBounds(this, this.position);
         };
+
+        Enemy.prototype.onDraw = function() {
+
+        }
 
         return Enemy;
     }
