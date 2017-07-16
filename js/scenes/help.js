ef68122b5cc23cfe04af72000c738a9b4206c9a8
Armag3ddon <Armag3ddon@web.de>
2017-07-16 11:50:25 +0200

    rhg

--- js/scenes/help.js
+++ js/scenes/help.js
@@ -1,13 +1,13 @@
 define(['lib/scene', 'entity/back', 'core/graphic'],
 		function(Scene, BackButton, g) {
 
-            g.add('img/menu_bg.png');
+            g.add('img/help_bg.jpg');
 
 			function HelpScene() {
 				Scene.call(this);
 				this.center(BackButton('menu'));
 
-                this.bg = 'img/menu_bg.png';
+                this.bg = 'img/help_bg.jpg';
 			}
 
 			HelpScene.prototype = new Scene();
